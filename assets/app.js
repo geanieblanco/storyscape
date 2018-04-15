3eae1439a8cce95282f0b511567b1e412214ca7d
geanie blanco <geanieblanco@gmail.com>
2018-04-15 19:42:50 -0400

    repo move

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+console.log(chance.bool());
+
+// first name function
+function generateFemale() {
+  chance.last({
+    nationality: "en"
+    console.log(this);
+  })
+  chance.first({
+    nationality: "en"
+    console.log(this);
+  })
+};
