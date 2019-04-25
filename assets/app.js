ea327f43cd215582679ff09b7c401710b6116161
Geanie Blanco <geanieblanco@gmail.com>
2019-04-25 08:06:31 -0400

    remove from local delete

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,50 @@
 console.log(chance.bool());
 
-// first name function
+// name functions
 function generateFemale() {
-  chance.last({
-    nationality: "en"
-    console.log(this);
+  var firstNameResponse = chance.first({
+    nationality: "en",
+    gender: "female"
   })
-  chance.first({
+  var lastNameResponse = chance.last({
     nationality: "en"
-    console.log(this);
   })
+  console.log(firstNameResponse + lastNameResponse);
 };
+
+function generateMale() {
+  var firstNameResponse = chance.first({
+    nationality: "en",
+    gender: "male"
+  })
+  var lastNameResponse = chance.last({
+    nationality: "en"
+  })
+  console.log(firstNameResponse + lastNameResponse);
+};
+
+function generateRandom() {
+  var firstNameResponse = chance.first({
+    nationality: "en"
+  })
+  var lastNameResponse = chance.last({
+    nationality: "en"
+  })
+  console.log(firstNameResponse + lastNameResponse);
+};
+
+function generateCharacter() {
+  var eyeColor = chance.pickone(['brown', 'black', 'blue', 'green', 'hazel']);
+  var hairColor = chance.pickone(['brown', 'black', 'blue', 'green', 'blonde', 'orange', 'pink', 'bald', 'purple', 'yellow', 'rainbow', 'mermaid', 'auburn', 'red', 'grey', 'white']);
+  var hairLength = chance.pickone(['extra long', 'long', 'medium', 'short', 'cropped']);
+  var skinTone;
+  var birthday = chance.birthday({ string: true });
+  var height = chance.integer({ min: 4, max: 6 });
+  var location = chance.state({ full: true });
+  console.log(eyeColor + hairColor);
+}
+
+generateFemale();
+generateMale();
+generateCharacter();
+generateRandom();
